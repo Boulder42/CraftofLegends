f24070640b23d3d55254bd4bb8451dd117b25c6b
Boulder42 <nathanrm262@gmail.com>
2015-01-26 18:03:22 -0800

    Initial Commit

---
+++ src/craftoflegends/net/craftoflegends/mod/blocks/VaniteOre.java
@@ -0,0 +1,29 @@
+package net.craftoflegends.mod.blocks;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.craftoflegends.mod.CraftofLegends;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+public class VaniteOre extends Block{
+
+	public VaniteOre(Material material) {
+		super(material);
+		
+		this.setHardness(3.0F);
+		this.setResistance(5.0F);
+		this.setStepSound(soundTypeStone);
+		this.setCreativeTab(getCreativeTabToDisplayOn().tabBlock);
+		
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister iconRegister) {
+		this.blockIcon = iconRegister.registerIcon(CraftofLegends.modid + ":" + this.getUnlocalizedName().substring(5));
+		
+	}
+
+	
+}
